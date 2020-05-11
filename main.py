f67defb7096406adf6feef5620d7b4124b6b7ee6
morinekiyohisa <64891242+morinekiyohisa@users.noreply.github.com>
2020-05-11 10:04:52 +0800

    Update main.py
    *優化註釋
    *四則計算機更新

--- main.py
+++ main.py
@@ -1,44 +1,57 @@
+#<------------------------------------------------------->
+#初始設置
+con = 0
 c = 0
-def cul(a,p,b):#四則計算機函數
-	if p == '+':
-		ans = a + b
-	elif p == '-':
-		ans = a - b
-	elif p == '*':
-		ans = a * b
-	elif p == '/':
-		ans = a / b
-	else:
-		print('請輸入四則運算')
-	return ans
-def cul2(a,p,b):
+#<------------------------------------------------------->
+def cul(a, p, b):  #四則計算機函數
+    if p == '+':
+        ans = a + b
+    elif p == '-':
+        ans = a - b
+    elif p == '*':
+        ans = a * b
+    elif p == '/':
+        ans = a / b
+    else:
+        print('請輸入四則運算')
+    return ans
+#<------------------------------------------------------->
+def cul2(a, p, b):
     if p == 1:
         ans = abs(a)
     elif p == 2:
-        ans = max(a,b)
+        ans = max(a, b)
     elif p == 3:
-        ans = min(a,b)
+        ans = min(a, b)
     return ans
-
+#<------------------------------------------------------->
 #功能選擇
-print('四則計算機=1','函數計算機=2',end='')
+print('四則計算機=1', '函數計算機=2', end='')
 c = int(input('請輸入需要的功能'))
-
+con = con + 1
+#<------------------------------------------------------->
 #四則計算機介面
-if c == 1:
-    a = int(input('數1='))
-    p = input('請輸入運算符號(若想退出此功能請在此輸入exit)')
-    b = int(input('數2='))
-    ans = cul(a,p,b)
-    print('答案是%2d'%(ans))
-elif c == 2:
-    a =int(input('a='))
-    b =int(input('b='))
-    print('我想算出')
-    print('a的絕對值=1')
-    print('a,b的最大值=2')
-    print('a,b的最小值=3')
-    print('重新設定=4')
-    p =int(input())
-    ans = cul2(a,p,b)
-    print('答案是%2d'%(ans))
+while con == 1:
+    while c == 1:
+        a = int(input('數1='))
+        p = input('請輸入運算符號(若想退出此功能請在此輸入exit)')
+        b = int(input('數2='))
+        ans = cul(a, p, b)
+        print('答案是%2d' % (ans))
+        print('要更改用函數計算機嗎?')
+        print('輸入1繼續', '輸入2')
+         = input()
+        if c == 1:
+            continue
+while con == 2
+        a = int(input('a='))
+        b = int(input('b='))
+        print('我想算出')
+        print('a的絕對值=1')
+        print('a,b的最大值=2')
+        print('a,b的最小值=3')
+        print('重新設定=4')
+        p = int(input())
+        ans = cul2(a, p, b)
+        print('答案是%2d' % (ans))
+#<------------------------------------------------------->
