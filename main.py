83d7826a3f051474781a75aef038f4e4dc4f41ec
morinekiyohisa <64891242+morinekiyohisa@users.noreply.github.com>
2020-05-12 15:01:45 +0800

    Update main.py
    *流程優化

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+switch = True
 #<------------------------------------------------------->
-def cul(a, b, p):#計算函數
+def calculate(a, b, p):#計算函數
     if p == '+':#四則運算
         ans = a + b
     elif p == '-':
@@ -25,14 +26,17 @@ def cul(a, b, p):#計算函數
         print('請輸入四則運算或想求出的值')
     return ans  #返回值
 #<------------------------------------------------------->
-print('請輸入數字')#計算機介面
-a = int(input('數1='))
-b = int(input('數2='))
-print('請輸入運算符號或想求出的值(若想退出此功能請在此輸入exit)')
-print('求a的x階乘=1')
-print('求a的x次方=2 數字2為次方數')
-print('重新設定=3')
-p = input()
-ans = cul(a, b, p)#呼叫函數
-print('答案是%2d' % (ans))
+while switch is True:
+    print('請輸入數字')#計算機介面
+    a = int(input('數1='))
+    b = int(input('數2='))
+    print('請輸入運算符號或想求出的值(若想退出此功能請在此輸入exit)')
+    print('求a的x階乘=1')
+    print('求a的x次方=2 數字2為次方數')
+    print('重新設定=3')
+    p = input()
+    if p == '3':
+      continue
+    ans = calculate(a, b, p)#呼叫函數
+    print('答案是%2d' % (ans))
 #<------------------------------------------------------->
